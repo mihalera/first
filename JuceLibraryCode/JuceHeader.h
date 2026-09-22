2ebd559a37cdb405db8c75661f0d2dd86ca01189
mihalera <mihalera@users.noreply.github.com>
2026-09-22 17:46:36 +0300

    add

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -22,12 +22,22 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_processors_headless/juce_audio_processors_headless.h>
 #include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_box2d/juce_box2d.h>
+#include <juce_build_tools/juce_build_tools.h>
 #include <juce_core/juce_core.h>
+#include <juce_cryptography/juce_cryptography.h>
 #include <juce_data_structures/juce_data_structures.h>
+#include <juce_dsp/juce_dsp.h>
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_javascript/juce_javascript.h>
+#include <juce_midi_ci/juce_midi_ci.h>
+#include <juce_opengl/juce_opengl.h>
+#include <juce_osc/juce_osc.h>
+#include <juce_product_unlocking/juce_product_unlocking.h>
+#include <juce_video/juce_video.h>
 
 
 #if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
