a75bf7c1d78d79d399e4a74ca273de539d58acff
mihalera <88746771+mihalera@users.noreply.github.com>
2026-09-22 22:39:21 +0300

    build: normalize VST3 output directory

--- Builds/VisualStudio2026/flatten-vst3.cmd
+++ Builds/VisualStudio2026/flatten-vst3.cmd
@@ -2,6 +2,7 @@
 setlocal
 
 set "outputDir=%~1"
+if not "%outputDir:~-1%"=="\\" set "outputDir=%outputDir%\\"
 set "bundle=%outputDir%first.vst3"
 set "binary=%bundle%\Contents\x86_64-win\first.vst3"
 set "temporary=%outputDir%first-flat-vst3.tmp"
