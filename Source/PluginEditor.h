32cd94161645e1864933348420afc13c19765e41
mihalera <88746771+mihalera@users.noreply.github.com>
2026-09-22 22:59:43 +0300

    refactor: define aligned editor controls and meter components

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,8 +1,6 @@
 /*
   ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
+    J37 tape processor editor.
   ==============================================================================
 */
 
@@ -12,27 +10,44 @@
 #include "PluginProcessor.h"
 
 #include <array>
+#include <memory>
 
 #include <juce_box2d/juce_box2d.h>
 #include <juce_opengl/juce_opengl.h>
 
-//==============================================================================
-/**
-*/
-class FirstAudioProcessorEditor  : public juce::AudioProcessorEditor,
-                                   private juce::Timer
+class J37LookAndFeel final : public juce::LookAndFeel_V4
 {
 public:
-    FirstAudioProcessorEditor (FirstAudioProcessor&);
+    void drawRotarySlider (juce::Graphics&,
+                           int, int, int, int,
+                           float, float, float,
+                           juce::Slider&) override;
+};
+
+class FirstAudioProcessorEditor final : public juce::AudioProcessorEditor,
+                                        private juce::Timer
+{
+public:
+    explicit FirstAudioProcessorEditor (FirstAudioProcessor&);
     ~FirstAudioProcessorEditor() override;
 
-    //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
-    void timerCallback() override;
-    void createDecorativePhysics();
+    class LevelMeter final : public juce::Component
+    {
+    public:
+        explicit LevelMeter (juce::String title);
+        void setLevels (float peakLinear, float rmsLinear);
+        void paint (juce::Graphics&) override;
+
+    private:
+        juce::String title;
+        float rmsDb = -60.0f;
+        float peakHoldDb = -60.0f;
+        float peakHoldTime = 0.0f;
+    };
 
     struct PhysicsOrb
     {
@@ -48,41 +63,55 @@ private:
         juce::Colour colour;
     };
 
+    struct EditorLayout
+    {
+        juce::Rectangle<int> header;
+        juce::Rectangle<int> deck;
+        juce::Rectangle<int> controls;
+        juce::Rectangle<int> meters;
+    };
+
+    static constexpr std::size_t controlCount = 8;
     static constexpr std::size_t decorativeOrbCount = 4;
 
+    void timerCallback() override;
+    void createDecorativePhysics();
+    EditorLayout getEditorLayout() const;
+
     FirstAudioProcessor& audioProcessor;
+    J37LookAndFeel customLookAndFeel;
+
+    std::array<juce::Slider, controlCount> controls;
+    std::array<juce::Label, controlCount> controlLabels;
+    std::array<std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>,
+               controlCount> controlAttachments;
+
+    juce::ComboBox tapeTypeBox;
+    juce::ComboBox speedBox;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> tapeTypeAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> speedAttachment;
+
+    juce::Label brandLabel;
+    juce::Label titleLabel;
+    juce::Label subtitleLabel;
+    juce::Label statusLabel;
+    juce::Label deckHeadingLabel;
+    juce::Label tapeTypeLabel;
+    juce::Label speedLabel;
+    juce::Label deckHintLabel;
+    juce::Label controlsHeadingLabel;
+    juce::Label controlsHintLabel;
+    juce::Label metersHeadingLabel;
+    juce::Label metersHintLabel;
+
+    LevelMeter inputMeter { "INPUT" };
+    LevelMeter outputMeter { "OUTPUT" };
+
     std::unique_ptr<b2World> physicsWorld;
     std::array<PhysicsOrb, decorativeOrbCount> physicsOrbs {};
     std::array<RenderOrb, decorativeOrbCount> renderOrbs {};
     juce::SpinLock renderOrbsLock;
     juce::OpenGLContext openGLContext;
-
-    juce::Slider driveSlider;
-    juce::Slider biasSlider;
-    juce::Slider toneSlider;
-    juce::Slider wowSlider;
-    juce::Slider flutterSlider;
-    juce::Slider mixSlider;
-    juce::Slider outputSlider;
-    juce::ComboBox tapeTypeBox;
-    juce::ComboBox speedBox;
-    juce::LookAndFeel_V4 customLookAndFeel;
-
-    using SliderAttachment =
-        juce::AudioProcessorValueTreeState::SliderAttachment;
-
-    using ComboBoxAttachment =
-        juce::AudioProcessorValueTreeState::ComboBoxAttachment;
-
-    std::unique_ptr<SliderAttachment> driveAttachment;
-    std::unique_ptr<SliderAttachment> biasAttachment;
-    std::unique_ptr<SliderAttachment> toneAttachment;
-    std::unique_ptr<SliderAttachment> wowAttachment;
-    std::unique_ptr<SliderAttachment> flutterAttachment;
-    std::unique_ptr<SliderAttachment> mixAttachment;
-    std::unique_ptr<SliderAttachment> outputAttachment;
-    std::unique_ptr<ComboBoxAttachment> tapeTypeAttachment;
-    std::unique_ptr<ComboBoxAttachment> speedAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FirstAudioProcessorEditor)
 };
